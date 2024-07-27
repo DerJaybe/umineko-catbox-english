c6f76e942212025237eda05e00adba74f53f2367
Jaybe-Games <derjaybe@gmail.com>
2024-07-27 17:16:16 +0200

    fix german characters and add optional textboxes
    I feel more and more pain

---
+++ replace_chars.js
@@ -0,0 +1,17 @@
+// This script replaces unusual (non-English) characters with their Unicode values.
+// Used during translations to other languages.
+
+const fs = require('fs')
+
+const intlCharacters = fs.readFileSync('replace_chars.txt', 'utf-8').split('\n').map(x => x.trim()).filter(x => x)
+
+let script = fs.readFileSync('script.rb', 'utf-8')
+
+for (const character of intlCharacters) {
+  const code = character.codePointAt()
+  const regex = new RegExp(character, 'g')
+  script = script.replace(regex, `@u${code}.`)
+}
+
+fs.writeFileSync('script.rb', script)
+
